4dd40425bd01ff526636cee38102f54b9b5640d7
yvasilyev92 <yvasilye@mail.sfsu.edu>
2017-08-19 18:23:13 -0700

    Final Exercise 5 complete

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Google Places API demo app
 
 ShushMe allows you to pick a set of locations using Google's Places API, it then creates geofences around those places and turns your device into silent mode if you enter any of them. Once the device exits again ShushMe turns your device back to normal (whatever ringer mode and volume you had before entering).
 
+This exercise project covers how Android calculates a device's location and how it sacrifices accuracy for lower battery usage.
+It also covers Google Places API components such as Geofencing and PlacePicker. As well as AudioManagers, device permissions,
+device ringers, BroadcastReceivers, RecyclerView Adapters, Google API Client, and Google Play Services.
+
 ## Screenshots
 
 ![Screenshot1](screenshots/screen_1.png) ![Screenshot2](screenshots/screen_2.png) ![Screenshot3](screenshots/screen_3.png)
--- app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
@@ -4,10 +4,19 @@ package com.example.android.shushme;
  * Created by Yevgeniy on 8/19/2017.
  */
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
@@ -21,8 +30,184 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
      * @param context
      * @param intent
      */
+
+
+
+
+
+
+
+
+    /*
+    In onReceive, we can use GeofencingEvent.fromIntent to retrieve the GeofencingEvent
+    that caused the transition. From that we can call getGeofenceTransition to get the transition type.
+    And based on the transition type, we can use AudioManager to set the phone ringer mode.
+     */
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "onReceive called");
+
+
+
+        // Get the Geofence Event from the Intent sent through
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            Log.e(TAG, String.format("Error code : %d", geofencingEvent.getErrorCode()));
+            return;
+        }
+
+
+
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+
+
+
+        // Check which transition type has triggered this event
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            setRingerMode(context, AudioManager.RINGER_MODE_SILENT);
+
+        } else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            setRingerMode(context, AudioManager.RINGER_MODE_NORMAL);
+
+        } else {
+
+            // Log the error.
+            Log.e(TAG, String.format("Unknown transition : %d", geofenceTransition));
+            // No need to do anything else
+            return;
+
+        }
+
+
+
+        // Send the notification
+        sendNotification(context, geofenceTransition);
     }
+
+
+
+
+
+
+
+
+    /**
+     * Posts a notification in the notification bar when a transition is detected
+     * Uses different icon drawables for different transition types
+     * If the user clicks the notification, control goes to the MainActivity
+     *
+     * @param context        The calling context for building a task stack
+     * @param transitionType The geofence transition type, can be Geofence.GEOFENCE_TRANSITION_ENTER
+     *                       or Geofence.GEOFENCE_TRANSITION_EXIT
+     */
+
+
+
+    /*
+    It would also be nice to have the app notify the user whenever the device
+    has been set to silent or back to normal.
+    So we create this helper method sendNotification that will
+    create a typical Android notification with an icon corresponding to
+    either being silent or normal.
+     */
+
+
+    private void sendNotification(Context context, int transitionType) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        // Check the transition type to display the relevant icon image
+        if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            builder.setSmallIcon(R.drawable.ic_volume_off_white_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.ic_volume_off_white_24dp))
+                    .setContentTitle(context.getString(R.string.silent_mode_activated));
+        } else if (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            builder.setSmallIcon(R.drawable.ic_volume_up_white_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.ic_volume_up_white_24dp))
+                    .setContentTitle(context.getString(R.string.back_to_normal));
+        }
+
+        // Continue building the notification
+        builder.setContentText(context.getString(R.string.touch_to_relaunch));
+        builder.setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Changes the ringer mode on the device to either silent or back to normal
+     *
+     * @param context The context to access AUDIO_SERVICE
+     * @param mode    The desired mode to switch device to, can be AudioManager.RINGER_MODE_SILENT or
+     *                AudioManager.RINGER_MODE_NORMAL
+     */
+
+
+
+    /*
+    To keep things modular. We create this helper method setRingerMode that using an AudioManager
+    can set the setRingerMode to either silent or normal based on the mode parameter passed in.
+    It also creates a NotificationManager to check for permissions for Android N or later.
+     */
+
+    private void setRingerMode(Context context, int mode) {
+
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Check for DND permissions for API 24+
+        if (android.os.Build.VERSION.SDK_INT < 24 ||
+                (android.os.Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted())) {
+
+            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(mode);
+        }
+    }
+
+
+
+
+
+
+
 }
